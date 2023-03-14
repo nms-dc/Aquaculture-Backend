29b31864a04598bdb027ff07973ed294412b6389
pugal <pugal.m@deductiveclouds.com>
2023-03-14 13:07:45 +0530

    farmcertificateChanges

--- backend/backend/apps/farms/api/urls.py
+++ backend/backend/apps/farms/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import URLPattern, path, include
-from farms.api.views import FarmView, FeedLotsView
+from farms.api.views import FarmView, FeedLotsView, CertifyView
 from rest_framework.routers import DefaultRouter
 
 app_name = "farms"
@@ -7,6 +7,7 @@ app_name = "farms"
 router = DefaultRouter()
 router.register('farmregist', FarmView)
 router.register('feedregist', FeedLotsView)
+router.register('certifyregist', CertifyView)
 
 urlpatterns = [
      path('', include(router.urls)),
