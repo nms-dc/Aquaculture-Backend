e3b0756086a6dd883f4600d46cef6ec982f5e59f
pugal <pugal.m@deductiveclouds.com>
2023-05-11 11:49:21 +0530

    feed_pics_debugged

--- backend/backend/apps/feeds/admin.py
+++ backend/backend/apps/feeds/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from feeds.models  import FeedType, Feeds
+from feeds.models  import FeedType, Feeds, FeedPics
 
 # Register your models here.
 admin.site.register(FeedType)
 admin.site.register(Feeds)
+admin.site.register(FeedPics)
