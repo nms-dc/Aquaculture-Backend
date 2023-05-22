0f71f5a1922efea1ec31d9fa9773e7f723d7c091
pugal <pugal.m@deductiveclouds.com>
2023-05-22 17:15:50 +0530

    measure_changes

--- backend/backend/apps/cycle/signals.py
+++ backend/backend/apps/cycle/signals.py
@@ -4,37 +4,38 @@ from measurements.models import Measurement, MeasurementMaster
 from feeds.models import Feeds, FeedType
 from cycle.models import Cycle
 from farms.models import FarmAnalytics, FeedLots
-#from cycle.api.serializers import MeasurementSerializer
+
 
 @receiver(post_save, sender=Cycle)
 def copy_measurements(sender, instance, created, *args, **kwargs):
     if instance.pond_transfered_from:
-        cycle_id = Cycle.objects.filter(Pond = instance.pond_transfered_from, is_active = True).first()
-        measurement_data = Measurement.objects.filter(cycle = cycle_id)
+        cycle_id = Cycle.objects.filter(Pond=instance.pond_transfered_from, is_active=True).first()
+        measurement_data = Measurement.objects.filter(cycle=cycle_id)
         measure_data = list(measurement_data.values())
         if measure_data:
             for measurement in measure_data:
-                # if measurement['measurement_type_id'] == 1  or measurement['measurement_type_id'] ==8:
-                master = MeasurementMaster.objects.filter(id=measurement['measurement_type_id'] ).first()
-                # lots = FeedLots.objects.filter(id=measurement['lot_id']).first()
-                if master.measurement_type =='abw':
+                master = MeasurementMaster.objects.filter(id=measurement['measurement_type_id']).first()
+                print('measure block', measurement)
+                if master.measurement_type == 'abw':
+                    print('measurement data creating')
                     Measurement.objects.create(
                         cycle=instance,
-                        measurement_type=master,
+                        measurement_type_id=master.id,
                         value=measurement['value'],
                         time=measurement['time'],
                         notes=measurement['notes'],
-						created_by=measurement['created_by'],
-						updated_by=measurement['updated_by']
+                        created_by_id=measurement['created_by_id'],
+                        updated_by_id=measurement['updated_by_id']
                     )
-					
-        # Now insert data from feeds 
-        feeding_data = Feeds.objects.filter(cycle = cycle_id)
+
+        feeding_data = Feeds.objects.filter(cycle=cycle_id)
         feed_data = list(feeding_data.values())
         if feed_data:
             for feed in feed_data:
-                master = FeedType.objects.filter(id=feed['feed_type_id'] ).first()
-                if master.type=='feeds':
+                master = FeedType.objects.filter(id=feed['feed_type_id']).first()
+                print('feed data')
+                if master.type == 'feeds':
+                    print('feed data creating')
                     Feeds.objects.create(
                         cycle=instance,
                         feed_type=master,
@@ -42,9 +43,8 @@ def copy_measurements(sender, instance, created, *args, **kwargs):
                         time=feed_data[0]['time'],
                         price_per_kg=feed_data[0]['price_per_kg'],
                         is_probiotic_mixed=feed_data[0]['price_per_kg'],
-                        #created_by=feed_data[0]['created_by'],
-                        #updated_by=feed_data[0]['updated_by']
+                        created_by_id=feed_data[0]['created_by_id'],
+                        updated_by_id=feed_data[0]['updated_by_id']
                     )
     else:
         print('measure_data-not found')
-    
