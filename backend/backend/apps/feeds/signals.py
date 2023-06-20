f3ff99c2b43d313235030a8bceef905fa40e848e
pugal001 <pugal.m@deductiveclouds.com>
2023-06-20 16:31:50 +0530

    signals_changes

--- backend/backend/apps/feeds/signals.py
+++ backend/backend/apps/feeds/signals.py
@@ -9,32 +9,33 @@ import pandas as pd
 
 
 
-# @receiver(post_save, sender=Feeds)
-# def compute_graph(sender, instance, created, *args, **kwargs):
-#     default_list = ['feeds']
-#     feed_type = instance.feed_type.type
-#     if instance.value is None:
-#         instance_value = 0
-#     else:
-#         instance_value = instance.value
-#     already_exists = None
-#     if not created:
-#         already_exists = PondGraphs.objects.filter(extra_info__feed_id=instance.id)
-#     if created and feed_type in default_list:
-#         PondGraphs.objects.create(farm=instance.cycle.Pond.farm,
-#                                   pond=instance.cycle.Pond,
-#                                   time=instance.time,
-#                                   abw=None,
-#                                   total_feed=instance_value if feed_type == default_list[0] else None,
-#                                   extra_info={'feed_id': instance.id}
-#                                   )
-#     elif feed_type in default_list and not created:
-#         pond_graph_instance = already_exists.first()
-#         pond_graph_instance.time = instance.time
-#         pond_graph_instance.abw = None
-#         pond_graph_instance.total_feed += float(instance_value) if feed_type == default_list[0] else None
-#         pond_graph_instance.save()
-
+@receiver(post_save, sender=Feeds)
+def compute_graph(sender, instance, created, *args, **kwargs):
+    default_list = ['feeds']
+    feed_type = instance.feed_type.type
+    if instance.value is None:
+        instance_value = 0
+    else:
+        instance_value = instance.value
+    already_exists = None
+    if not created:
+        already_exists = PondGraphs.objects.filter(extra_info__feed_id=instance.id)
+    if created and feed_type in default_list:
+        PondGraphs.objects.create(farm=instance.cycle.Pond.farm,
+                                  pond=instance.cycle.Pond,
+                                  time=instance.time,
+                                  abw=None,
+                                  total_feed=instance_value if feed_type == default_list[0] else None,
+                                  extra_info={'feed_id': instance.id}
+                                  )
+    elif feed_type in default_list and not created:
+        if already_exists.exists():
+            pond_graph_instance = already_exists.first()
+            pond_graph_instance.time = instance.time
+            #pond_graph_instance.abw = None
+            pond_graph_instance.total_feed += float(instance_value) if feed_type == default_list[0] else None
+            pond_graph_instance.save()
+        
 
 @receiver(post_save, sender=Feeds)
 def compute_analytics(sender, instance, created, *args, **kwargs):
@@ -82,43 +83,35 @@ def compute_analytics(sender, instance, created, *args, **kwargs):
                                       total_probiotics=instance_value,
                                       harvest_amount=0,
                                       extra_info={'feed_id': instance.id})
-    # already_exists_pond = PondAnalytics.objects.filter(pond=instance.cycle.Pond, farm=instance.cycle.Pond.farm)
-    # pond_cycles = Cycle.objects.filter(Pond=instance.cycle.Pond).values()
-    # cycle_ids = []
-    # for cycle in pond_cycles:
-    #     cycle_ids.append(cycle['id'])
-    
-    # all_feed_value = 0
-    # for id in cycle_ids:
-    #     feed_value = Feeds.objects.filter(cycle=id).values()
-    
-    #     exists_feeds = 0
-    #     exists_probiotics = 0
-    #     if feed_type == default_list[0]:
-    #         df = pd.DataFrame(feed_value)
-    #         exists_feeds = df['value'].sum()
-    #         all_feed_value += exists_feeds
-    # print('pond_feed',all_feed_value)
-    # if already_exists_pond.exists() and feed_type == default_list[0]:
-    #     pond_analytics_instance = already_exists_pond.first()
-    #     pond_analytics_instance.total_feed += instance.value
-    #     pond_analytics_instance.save()
-    # elif not already_exists_pond.exists() and feed_type == default_list[0]:
-    #     PondAnalytics.objects.create(farm=instance.cycle.Pond.farm,
-    #                                  pond=instance.cycle.Pond,
-    #                                  total_feed=instance_value,
-    #                                  harvest_amount=0,
-    #                                  extra_info={'feed_id': instance.id})
-    # already_exists_farm = FarmAnalytics.objects.filter(farm=instance.cycle.Pond.farm)
-    # if already_exists_farm.exists() and feed_type == default_list[0]:
-    #     farm_analytics_instance = already_exists_farm.first()
-    #     farm_analytics_instance.total_feed = farm_analytics_instance.total_feed + instance_value
-    #     farm_analytics_instance.save()
-    # elif not already_exists_farm.exists() and feed_type == default_list[0]:
-    #     FarmAnalytics.objects.create(farm=instance.cycle.Pond.farm,
-    #                                  total_feed=instance_value,
-    #                                  harvest_amount=0,
-    #                                  extra_info={'feed_id': instance.id})
+    already_exists_pond = PondAnalytics.objects.filter(pond=instance.cycle.Pond, farm=instance.cycle.Pond.farm)
+    total_cycles = CycleAnalytics.objects.filter(pond = instance.cycle.Pond).values()
+    #print(total_cycles)
+    total_feed_df = pd.DataFrame(total_cycles)
+    total_feed = total_feed_df['total_feed'].sum()
+    if already_exists_pond.exists() and feed_type == default_list[0]:
+        pond_analytics_instance = already_exists_pond.first()
+        pond_analytics_instance.total_feed = total_feed
+        pond_analytics_instance.save()
+    elif not already_exists_pond.exists() and feed_type == default_list[0]:
+        PondAnalytics.objects.create(farm=instance.cycle.Pond.farm,
+                                     pond=instance.cycle.Pond,
+                                     total_feed=instance_value,
+                                     harvest_amount=0,
+                                     extra_info={'feed_id': instance.id})
+    already_exists_farm = FarmAnalytics.objects.filter(farm=instance.cycle.Pond.farm)
+    PondAnalytics_total = PondAnalytics.objects.filter(farm = instance.cycle.Pond.farm).values()
+    print(PondAnalytics_total)
+    pondanalytics_df = pd.DataFrame(PondAnalytics_total)
+    total_pondanalytics_feed = pondanalytics_df['total_feed'].sum()
+    if already_exists_farm.exists() and feed_type == default_list[0]:
+        farm_analytics_instance = already_exists_farm.first()
+        farm_analytics_instance.total_feed = total_pondanalytics_feed
+        farm_analytics_instance.save()
+    elif not already_exists_farm.exists() and feed_type == default_list[0]:
+        FarmAnalytics.objects.create(farm=instance.cycle.Pond.farm,
+                                     total_feed=instance_value,
+                                     harvest_amount=0,
+                                     extra_info={'feed_id': instance.id})
 
 
 @receiver(post_delete, sender=Feeds)
--- backend/backend/apps/harvests/signals.py
+++ backend/backend/apps/harvests/signals.py
@@ -4,6 +4,7 @@ from harvests.models import Harvests
 from ponds.models import PondGraphs, PondAnalytics
 from cycle.models import CycleAnalytics
 from farms.models import FarmAnalytics
+import pandas as pd
 
 
 @receiver(post_save, sender=Harvests)
@@ -14,10 +15,16 @@ def compute_harvest_analytics(sender, instance, created, *args, **kwargs):
         instance_total_kgs = instance.total_kgs
     already_exists_cycle = CycleAnalytics.objects.filter(cycle=instance.cycle, pond=instance.cycle.Pond,
                                                          farm=instance.cycle.Pond.farm)
+    
+    #getting all the harvest connected to the cycle and summarize the harvest_amount in each harvest for cycle analytics
+    same_cycle_harvests = Harvests.objects.filter(cycle = instance.cycle).values()
+    total_harvest_df = pd.DataFrame(same_cycle_harvests)
+    #print(total_harvest_df['total_kgs'])
+    total_kgs_sum = total_harvest_df['total_kgs'].sum()
 
     if already_exists_cycle.exists():
         cycle_analytics_instance = already_exists_cycle.first()
-        cycle_analytics_instance.harvest_amount += instance_total_kgs
+        cycle_analytics_instance.harvest_amount = total_kgs_sum
         cycle_analytics_instance.save()
     elif not already_exists_cycle.exists():
         CycleAnalytics.objects.create(farm=instance.cycle.Pond.farm,
@@ -27,9 +34,14 @@ def compute_harvest_analytics(sender, instance, created, *args, **kwargs):
                                       harvest_amount=instance_total_kgs,
                                       extra_info={'harvest_id': instance.id})
     already_exists_pond = PondAnalytics.objects.filter(pond=instance.cycle.Pond, farm=instance.cycle.Pond.farm)
+    #getting all the cycleAnalytics connected to the pond and summarize the harvest_amount in each cycle analytics for pond analytics
+    same_pond_cycle_analytics = CycleAnalytics.objects.filter(pond=instance.cycle.Pond).values()
+    cycle_analytics_df = pd.DataFrame(same_pond_cycle_analytics)
+    total_harvest_amount = cycle_analytics_df['harvest_amount'].sum()
+    #print(same_pond_cycle_analytics)
     if already_exists_pond.exists():
         pond_analytics_instance = already_exists_pond.first()
-        pond_analytics_instance.harvest_amount += instance_total_kgs
+        pond_analytics_instance.harvest_amount = total_harvest_amount
         pond_analytics_instance.save()
     elif not already_exists_pond.exists():
         PondAnalytics.objects.create(farm=instance.cycle.Pond.farm,
@@ -39,9 +51,14 @@ def compute_harvest_analytics(sender, instance, created, *args, **kwargs):
                                      extra_info={'harvest_id': instance.id})
 
     already_exists_farm = FarmAnalytics.objects.filter(farm=instance.cycle.Pond.farm)
+    #getting all the pondAnalytics connected to the farm and summarize the harvest_amount in each pond analytics for farm analytics
+    same_farm_pond_analytics = PondAnalytics.objects.filter(farm=instance.cycle.Pond.farm).values()
+    pond_analytics_df = pd.DataFrame(same_farm_pond_analytics)
+    total_harvest_amount = pond_analytics_df['harvest_amount'].sum()
+    print(same_farm_pond_analytics)
     if already_exists_farm.exists():
         farm_analytics_instance = already_exists_farm.first()
-        farm_analytics_instance.harvest_amount += instance_total_kgs
+        farm_analytics_instance.harvest_amount = total_harvest_amount
         farm_analytics_instance.save()
     elif not already_exists_farm.exists():
         FarmAnalytics.objects.create(farm=instance.cycle.Pond.farm,
