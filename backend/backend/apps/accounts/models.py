dc31849716379e9fbe9686ad57a2caf1550fcf28
NS <narayana.s@deductiveclouds.com>
2023-07-19 15:33:39 +0530

    Implenenting SendGrid

--- backend/backend/apps/accounts/models.py
+++ backend/backend/apps/accounts/models.py
@@ -16,6 +16,10 @@ from sendgrid import SendGridAPIClient
 #from typing import List, Dict
 from django.conf import settings
 
+from accounts.services.emails import (
+    AdminVerifiedEmailTemplate, SignUpAcceptedEmailTemplate
+)
+
 
 
 
@@ -135,15 +139,22 @@ class User(AbstractBaseUser):
         # Simplest possible answer: All admins are staff but not normal user
         return self.is_verified
 
-    #def save(self, *args, **kwargs):
-        # from django.core.mail import send_mail
-        # if self.is_verified == True:
-        #     send_mail('hi subject maded by deductiveclouds',f'congrats your mail has been verified {self.email}','pugal.m@deductiveclouds.com',[self.email],fail_silently=False)
-        #     print('mail sent successfully')
-        # else:
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+        if self.is_verified == True:
+            AdminVerifiedEmailTemplate.send_email(
+            subject="Success! Your Account has been verified.",
+            email_receivers=[self.email],
+            instance=self,
+            )
+        else:
+            SignUpAcceptedEmailTemplate.send_email(
+            subject="Success! New User account has been created",
+            email_receivers=[settings.ADMIN_EMAIL_1, settings.ADMIN_EMAIL_2, settings.ADMIN_EMAIL_3, settings.ADMIN_EMAIL_4],
+            instance=self,
+            )
 
-        #     send_mail('hi subject maded by deductiveclouds',f'a new user logged in and his mail id is {self.email}','pugal.m@deductiveclouds.com',['narayana.s@deductiveclouds.com'],fail_silently=False)
-        #     print('mail sent successfully')
+
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance = None, created=False, **kwargs):
     #if new  user has  been created we want to generate a token
